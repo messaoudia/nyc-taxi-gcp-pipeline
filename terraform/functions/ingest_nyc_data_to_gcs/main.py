d1f10b08d4db50d786ef1d740d1053d5424ea695
Amin Messaoudi <amin_messaoudi@yahoo.com>
2026-03-11 23:22:09 +0100

    update code to comply to cloud run jobs logic

--- terraform/functions/ingest_nyc_data_to_gcs/main.py
+++ terraform/functions/ingest_nyc_data_to_gcs/main.py
@@ -1,57 +1,96 @@
 import os
-import flask
+import json
+import logging
 import requests
-from google.cloud import pubsub_v1, storage
+from google.cloud import pubsub_v1, storage, parametermanager_v1
 
 
-def run(request: flask.Request):
-    """HTTP Cloud Function to simulate events by sending message to a Pub/Sub topic.
-
-    Args:
-        request (flask.Request): request data which will contain the end of the URI of the data to send to Pub/Sub
+def run():
     """
-    print(f"Received event: {request.args}")
+    Cloud Run Job - Ingest NYC Taxi data from a public URL
+    and upload it sreamingly to a GCS bucket + publish a message to Pub/Sub with the file information
+
+    Environment variables expected:
+    - CLOUD_RUN_TASK_INDEX: The index of the Cloud Run task: given by GCP
+    - GOOGLE_CLOUD_PROJECT: The GCP project ID
+    - BUCKET_NAME: The name of the GCS bucket where the data will be uploaded
+    - TOPIC_NAME: The name of the Pub/Sub topic where the message will be published
+    - NYC_DATA_URI: The base URI of the NYC taxi data files
+    - ENVIRONMENT: The environment (dev, staging,prod)
+    """
+
     # Environment variables for the function
-    cloud_run_task_index = os.getenv("CLOUD_RUN_TASK_INDEX")
+    cloud_run_task_index = int(os.getenv("CLOUD_RUN_TASK_INDEX", "0"))
     project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
     bucket_name = os.getenv("BUCKET_NAME")
     topic_name = os.getenv("TOPIC_NAME")
     nyc_data_uri = os.getenv("NYC_DATA_URI")
+    environment = os.getenv("ENVIRONMENT")
 
-    #
-    nyc_data_parquet = request.get_json().get("nyc_data_parquet")
-
-    if nyc_data_parquet is None:
-        return "Missing 'nyc_data_parquet' query parameter", 400
-    # request.get(nyc_data_parquet)
-
-    complete_nyc_data_uri = f"{nyc_data_uri}{nyc_data_parquet}"
-    response = requests.get(complete_nyc_data_uri)
-
-    if response.status_code != 200:
-        return f"Failed to access the data at {complete_nyc_data_uri}", 400
-
-    data = response.content
     storage_client = storage.Client()
 
-    storage_client.bucket(bucket_name).blob(
-        f"raw/{nyc_data_parquet}"
-    ).upload_from_string(data)
+    parameter_manager_client = parametermanager_v1.ParameterManagerClient()
+    parameter_name = f"projects/{project_id}/locations/global/parameters/nyc-taxi-gcp-pipeline-{environment}-parameter/versions/latest"
+
+    nyc_files = json.loads(
+        parameter_manager_client.render_parameter_version(
+            request=parametermanager_v1.RenderParameterVersionRequest(
+                name=parameter_name
+            )
+        ).payload.data
+    ).get("nyc_files")
+
+    if nyc_files is None:
+        raise ValueError(
+            "The 'nyc_files' parameter is missing in the Parameter Manager."
+        )
+
+    try:
+        nyc_data_parquet = nyc_files[cloud_run_task_index]
+    except IndexError as e:
+        error_message = (
+            f"""
+                Invalid number of CLOUD_RUN_TASK_INDEX, there is not enough nyc_files: {cloud_run_task_index}
+                Error: {str(e)}
+            """,
+        )
+        logging.error(error_message)
+        raise e
+
+    complete_nyc_data_uri = f"{nyc_data_uri}{nyc_data_parquet}"
+
+    blob = storage_client.bucket(bucket_name).blob(f"raw/{nyc_data_parquet}")
+    if blob.exists():
+        logging.info(
+            f"The file raw/{nyc_data_parquet} already exists in the bucket {bucket_name}. Skipping download and upload."
+        )
+        return
+
+    with requests.get(complete_nyc_data_uri, stream=True, timeout=600) as response:
+        response.raise_for_status()
+
+        blob.upload_from_file(
+            response.raw,
+            content_type="application/octet-stream",
+        )
 
     publisher = pubsub_v1.PublisherClient()
     topic_full_path = f"projects/{project_id}/topics/{topic_name}"
+    data = json.dumps(
+        {
+            "bucket": bucket_name,
+            "file_name": f"raw/{nyc_data_parquet}",
+        }
+    ).encode("utf-8")
 
-    future = publisher.publish(
-        topic_full_path, b"My first message ... :D", nyc_data_uri=complete_nyc_data_uri
-    )
+    future = publisher.publish(topic_full_path, data=data)
 
     future.result()  # Wait for the publish call to complete
 
-    print(
+    logging.info(
         f"Publishing message to Pub/Sub topic: {topic_name} with data: {complete_nyc_data_uri}"
     )
 
-    return (
-        f"Message published to Pub/Sub topic: {topic_name} with data: {complete_nyc_data_uri}",
-        200,
-    )
+
+if __name__ == "__main__":
+    run()
