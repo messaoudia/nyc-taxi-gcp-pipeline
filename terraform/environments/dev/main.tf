9dbca08b4a3c07187ba97f70c100d7ab0bafa78e
Amin Messaoudi <amin_messaoudi@yahoo.com>
2026-03-13 12:12:41 +0100

    add parameter manager, transform cloud run function to cloud run job, adding service account conf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -40,7 +40,7 @@ module "gcs_functions" {
 }
 
 resource "google_service_account" "cloud_run_job_service_account" {
-  account_id   = "${var.project_id}-function-sa"
+  account_id   = "${var.project_id}-sa"
   display_name = "Service Account for Cloud Functions"
 }
 
@@ -48,23 +48,56 @@ resource "google_storage_bucket_iam_member" "cloud_run_job_bucket_access" {
   bucket = module.gcs_data.bucket.name
   role   = "roles/storage.objectCreator"
   member = "serviceAccount:${google_service_account.cloud_run_job_service_account.email}"
+  depends_on = [
+    module.gcs_data,
+    google_service_account.cloud_run_job_service_account,
+  ]
 }
 
-resource "google_parameter_manager_iam_member" "function_parameter_access" {
-  parameter = google_parameter_manager_parameter.default.name
-  role      = "roles/parametermanager.parameterAccessor"
-  member    = "serviceAccount:${google_service_account.cloud_run_job_service_account.email}"
+resource "google_secret_manager_secret_iam_member" "cloud_run_job_secret_access" {
+  secret_id = google_secret_manager_secret.secrets.secret_id
+  role   = "roles/secretmanager.secretAccessor"
+  member = "serviceAccount:${google_service_account.cloud_run_job_service_account.email}"
+  depends_on = [
+    google_secret_manager_secret.secrets,
+    google_service_account.cloud_run_job_service_account,
+  ]
 }
 
 resource "google_service_account_iam_member" "cloud_run_job_user" {
   service_account_id = google_service_account.cloud_run_job_service_account.name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${google_service_account.cloud_run_job_service_account.email}"
+
+  depends_on = [
+    google_service_account.cloud_run_job_service_account,
+  ]
 }
 
 module "pubsub" {
   source     = "../../modules/pubsub"
   topic_name = "${var.project_id}-${var.environment}-${var.topic_name}"
+}
+
+resource "google_secret_manager_secret" "secrets" {
+  project = var.project_id
+  secret_id = "${var.project_id}-${var.environment}-secrets"
+  replication {
+    user_managed {
+      replicas {
+        location = "europe-west1"
+      }
+    }
+  }
+}
+
+# This is not best practice to store non-sensitive data in Secret Manager
+# But for the demo purpose, we will store the NYC data URI in Secret Manager to demonstrate how to use secrets in Cloud Run jobs
+resource "google_secret_manager_secret_version" "secrets_version" {
+  secret      = google_secret_manager_secret.secrets.name
+  secret_data = jsonencode({
+    "nyc_data_uri": var.nyc_data_uri
+  })
 }
 
 resource "google_parameter_manager_parameter" "default" {
@@ -78,42 +111,72 @@ resource "google_parameter_manager_parameter_version" "default" {
   parameter_data = jsonencode({
       "nyc_files": [
       "yellow_tripdata_2025-01.parquet",
-      "yellow_tripdata_2025-02.parquet"
+      "yellow_tripdata_2025-02.parquet",
+      "yellow_tripdata_2025-03.parquet",
+      "yellow_tripdata_2025-04.parquet",
     ]
   })
 }
 
+resource "google_artifact_registry_repository" "repository" {
+  provider = google
+  repository_id = "${var.project_id}-${var.environment}-repository"
+  location = var.location
+  format = "DOCKER"
+}
+
+locals {
+  docker_image_uri = "${var.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repository.repository_id}/nyc-taxi-ingester-dev:latest"
+}
+
 # TODO transform to cloud run + retry policy + monitoring + alerting
-resource "google_cloudfunctions2_function" "simulate_events_function" {
-  name = "${var.project_id}-simulate-events-function-${var.topic_name}" # TODO make the function name to variable
-  description = "Cloud Function to simulate NYC taxi trip events"
+resource "google_cloud_run_v2_job" "ingest_nyc_data_to_gcs" {
+  name = "${var.project_id}-${var.environment}-ingest-nyc-data-to-gcs-job"
   location = var.location
 
-  build_config {
-    runtime = "python313"
-    entry_point = "trigger"
-    source {
-      storage_source {
-        bucket = module.gcs_functions.bucket.name
-        object = google_storage_bucket_object.simulate_function_source.name
+  deletion_protection = false # set to "true" in production check https://docs.cloud.google.com/run/docs/create-jobs?hl=fr#terraform
+
+  template {
+    template {
+        service_account = google_service_account.cloud_run_job_service_account.email
+        containers {
+        image = local.docker_image_uri
+        env {
+          name = "GOOGLE_CLOUD_PROJECT"
+          value = var.project_id
+        }
+        env {
+          name = "BUCKET_NAME"
+          value = module.gcs_data.bucket.name
+        }
+        env {
+          name = "TOPIC_NAME"
+          value = module.pubsub.topic.name
+        }
+        env {
+          name = "ENVIRONMENT"
+          value = var.environment
+        }
+        env {
+          name = "NYC_DATA_SECRET"
+          value_source {
+            secret_key_ref {
+              secret = google_secret_manager_secret.secrets.secret_id
+              version = "latest"
+            }
+          }
+        }
       }
     }
   }
 
-  service_config {
-    available_memory   = "256M"
-    timeout_seconds    = 60
+  depends_on = [
+    google_storage_bucket_iam_member.cloud_run_job_bucket_access,
+    google_service_account_iam_member.cloud_run_job_user,
+    google_secret_manager_secret_version.secrets_version,
+    google_parameter_manager_parameter_version.default,
+    google_artifact_registry_repository.repository,
+  ]
 
-    environment_variables = {
-      GOOGLE_CLOUD_PROJECT = var.project_id
-      BUCKET_NAME = module.gcs_data.bucket.name
-      TOPIC_NAME = module.pubsub.topic.name
-      NYC_DATA_URI = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
-      # https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet
-    }
-
-    all_traffic_on_latest_revision = true # newer versions of function will receive all traffic
-    # ingress_settings               = "ALLOW_INTERNAL_ONLY"
-    ingress_settings = "ALLOW_ALL" # for dev only
-  }
 }
+
