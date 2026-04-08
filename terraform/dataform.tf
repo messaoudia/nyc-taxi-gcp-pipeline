243490008973ef1b8ba6d57e4e0fa2eab769c55d
Amin Messaoudi <amin_messaoudi@yahoo.com>
2026-04-08 18:44:45 +0200

    reorganize project

---
+++ terraform/dataform.tf
@@ -0,0 +1,13 @@
+# gitbub repository for dataform
+resource "google_dataform_repository" "dataform_repo" {
+  project     = var.project_id
+  location    = var.region
+  repository_id = "${var.app_name}-dataform-repo-${var.environment}"
+  display_name = "${var.app_name} Dataform Repository ${var.environment}"
+  description  = "Dataform repository for ${var.app_name} in ${var.environment} environment"
+
+  git_remote_settings {
+    url = var.repository_url
+    default_branch = var.repository_default_branch
+    authentication_token
+}
