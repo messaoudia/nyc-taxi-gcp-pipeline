651c94c1df936419eb2af12fc513fea17b7b61d7
Amin Messaoudi <amin_messaoudi@yahoo.com>
2026-04-08 18:36:23 +0200

    removing useless prod folder

--- terraform/environments/prod/artifact_registry.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "repository" {
-  provider = google
-  repository_id = "${var.app_name}-repository-${var.environment}"
-  location = var.location
-  format = "DOCKER"
-}
